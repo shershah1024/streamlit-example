1b0f9aa0963f57de1e6dec228aa0c03e7abc46c2
shershah1024 <125534182+shershah1024@users.noreply.github.com>
2023-10-04 17:42:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Title of the page
+st.title("YouTube and Quizizz Embed Page")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# YouTube Embed
+st.header("Embed a YouTube Video")
+video_id = st.text_input("Enter the YouTube video ID (e.g., dQw4w9WgXcQ):")
+if video_id:
+    video_url = f"https://www.youtube.com/embed/{video_id}"
+    st.markdown(f'<iframe width="560" height="315" src="{video_url}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Quizizz Embed
+st.header("Embed a Quiz from Quizizz")
+quiz_id = st.text_input("Enter the Quizizz quiz ID:")
+if quiz_id:
+    quiz_url = f"https://quizizz.com/join/quiz/{quiz_id}/start"
+    st.markdown(f'<iframe src="{quiz_url}" width="100%" height="600px" frameborder="0"></iframe>', unsafe_allow_html=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.sidebar.text("Made with Streamlit")
