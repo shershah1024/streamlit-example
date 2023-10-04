e35e1521c0fd399d7d94a189184e23a32a4f9157
shershah1024 <125534182+shershah1024@users.noreply.github.com>
2023-10-04 18:05:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 
 # Title of the page
-st.title("YouTube and Quizizz Embed Page")
+st.title("YouTube and Quizizz Page")
+
+# Hardcoded YouTube and Quizizz embeds
+video_id = "6v0Q-2IhqrE&t"
+quiz_id = "651d53d3981f921f5ce77a40"
 
 # YouTube Embed
-st.header("Embed a YouTube Video")
-video_id = st.text_input("Enter the YouTube video ID (e.g., dQw4w9WgXcQ):")
-if video_id:
-    video_url = f"https://www.youtube.com/embed/{video_id}"
-    st.markdown(f'<iframe width="560" height="315" src="{video_url}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
+video_url = f"https://www.youtube.com/embed/{video_id}"
+st.markdown(f'<iframe width="100%" height="315" src="{video_url}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+# Spacer
+st.write(" ")
 
 # Quizizz Embed
-st.header("Embed a Quiz from Quizizz")
-quiz_id = st.text_input("Enter the Quizizz quiz ID:")
-if quiz_id:
-    quiz_url = f"https://quizizz.com/join/quiz/{quiz_id}/start"
-    st.markdown(f'<iframe src="{quiz_url}" width="100%" height="600px" frameborder="0"></iframe>', unsafe_allow_html=True)
+quiz_url = f"https://quizizz.com/join/quiz/{quiz_id}/start"
+st.markdown(f'<iframe src="{quiz_url}" width="100%" height="600px" frameborder="0"></iframe>', unsafe_allow_html=True)
 
 st.sidebar.text("Made with Streamlit")
